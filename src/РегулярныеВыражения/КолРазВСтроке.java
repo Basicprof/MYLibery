176ad6de6c0f034fee448252c76e1b9740be66e9
Basicprof <Basicprof@gmail.com>
2018-03-07 19:43:48 +0200

    07,03,2018

---
+++ src/РегулярныеВыражения/КолРазВСтроке.java
@@ -0,0 +1,26 @@
+package РегулярныеВыражения;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+//Методы start и end
+//        Далее представлен пример, в котором производится подсчет количества раз, когда в строке
+//        ввода встречается слово "кот".
+public class КолРазВСтроке {
+
+    private static final String REGEX = "\\bкот\\b";
+    private static final String INPUT = "кот кот кот котёл кот";
+
+    public static void main( String args[] ) {
+        Pattern p = Pattern.compile(REGEX);
+        Matcher m = p.matcher(INPUT);   // получение matcher объекта
+        int count = 0;
+
+        while(m.find()) {
+            count++;
+            System.out.println("Номер вхождения: "+count);
+            System.out.println("Начальная позиция вхождения: "+m.start());
+            System.out.println("Конечная позиция вхождения: "+m.end());
+        }
+    }
+}
+
--- src/СТРОКИ/Stroki.java
+++ src/СТРОКИ/Stroki.java
@@ -40,5 +40,20 @@ public class Stroki {
         // Преобразуем число чар будеон все в строку в строку
         System.out.println("String.copyValueOf = " + String.copyValueOf(arr));
         // String.copyValueOf  Принимает массив чаров и преобразует их в строку.
-    }
+
+
+        String str1 = "Добро пожаловать на ProgLang.su";
+        String str2 = "Сайт для изучения программирования";
+        StringBuffer str3 = new StringBuffer("Добро пожаловать на ProgLang.su");
+        StringBuffer str4 = new StringBuffer("Добро пожаловать");
+
+        boolean  result = str1.contentEquals(str3);
+        System.out.println("Строка \"Добро пожаловать на ProgLang.su\" равна строке буфера \"Добро пожаловать на ProgLang.su\"? \n" + result);
+
+        result = str2.contentEquals(str3);
+        System.out.println("Строка \"Сайт для изучения программирования\" равна строке буфера \"Добро пожаловать на ProgLang.su\"? \n" + result);
+
+        result = str1.contentEquals(str4);
+        System.out.println("Строка \"Добро пожаловать на ProgLang.su\" равна строке буфера \"Добро пожаловать\"? \n" + result);
+    } }
 }
