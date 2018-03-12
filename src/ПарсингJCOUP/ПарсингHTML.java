11f516f76d3736f2559f8619fed8790ef5e25fe0
Igor <Basicprof@gmail.com>
2018-03-12 14:27:17 +0200

    12,03,2018

---
+++ src/ПарсингJCOUP/ПарсингHTML.java
@@ -0,0 +1,27 @@
+package ПарсингJCOUP;
+
+public class ПарсингHTML {
+    public static void main(String[] args) {
+    List<Article> articleList = new ArrayList<>();
+    Document document =Jsoup.connect("http://4pda.ru"),get();
+    Elements elements = doc.getEleme
+    }
+   class Article {
+        private String url;
+        private String name;
+          public Article(String url,String name){
+              this.url =url;
+              this.name =name;
+          }
+        public String setName() {
+         }
+  public String setUrl() {
+        }
+        public String getUrl() {
+            return url;
+        }
+        public String getName() {
+            return name;
+        }
+      }
+}
