c658e5e2e6705c61649d7c8e088e3b2fe35414bd
Igor <Basicprof@gmail.com>
2018-03-16 16:42:36 +0200

    Решение 16,03,2018

---
+++ src/ПАРСИНГ/ПарсингJCOUP/ПарсингHTML.java
@@ -0,0 +1,28 @@
+package ПАРСИНГ.ПарсингJCOUP;
+
+
+public class ПарсингHTML {
+    public static void main(String[] args) {
+    List<Article> articleList = new ArrayList<>();
+    Document document = Jsoup.connect("http://4pda.ru").get();
+    Elements elements = doc.getElemet
+    }
+   class Article {
+        private String url;
+        private String name;
+          public Article(String url,String name){
+              this.url =url;
+              this.name =name;
+          }
+        public String setName() {
+         }
+  public String setUrl() {
+        }
+        public String getUrl() {
+            return url;
+        }
+        public String getName() {
+            return name;
+        }
+      }
+}
