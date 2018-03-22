74467faa859317aaa21f697e33cf8b48d395b40b
Basicprof <Basicprof@gmail.com>
2018-03-22 22:48:05 +0200

    22,03

--- src/ПарсингJCOUP/ПарсингHTML.java
+++ src/ПарсингJCOUP/ПарсингHTML.java
@@ -1,11 +1,16 @@
 package ПарсингJCOUP;
 
 
+import javax.lang.model.util.Elements;
+import javax.swing.text.Document;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ПарсингHTML {
     public static void main(String[] args) {
     List<Article> articleList = new ArrayList<>();
-    Document document = Jsoup.connect("http://4pda.ru").get();
-    Elements elements = doc.getElemet
+   // Document document = Jsoup.connect("http://4pda.ru").get();
+   // Elements elements = doc.getElemet
     }
    class Article {
         private String url;
@@ -15,9 +20,9 @@ public class ПарсингHTML {
               this.name =name;
           }
         public String setName() {
-         }
+ return null;        }
   public String setUrl() {
-        }
+        return null;}
         public String getUrl() {
             return url;
         }
