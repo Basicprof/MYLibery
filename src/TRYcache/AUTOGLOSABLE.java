ac7ae8c979e50718106103c92383f3cc9946804d
Basicprof <Basicprof@gmail.com>
2018-03-27 16:36:53 +0300

    27,03.2018

---
+++ src/TRYcache/AUTOGLOSABLE.java
@@ -0,0 +1,46 @@
+package TRYcache;
+
+public class AUTOGLOSABLE {
+    public static void main(String[] args) {
+
+
+        DBConnectionManager dbConnectionManager = new DBConnectionManager();
+        try (FakeConnection fakeConnection = dbConnectionManager.getFakeConnection()) {
+            System.out.println("Entering the body of try block.");
+            fakeConnection.usefulOperation();
+
+            fakeConnection.unsupportedOperation();
+
+
+        } catch (Exception e) {
+        }
+    }
+    public static class FakeConnection implements AutoCloseable
+    {
+
+        public FakeConnection() {
+            System.out.println("Creating database connection...");
+        }
+
+        public void unsupportedOperation() {
+            System.out.println("Operation is not supported yet!");
+            throw new RuntimeException("UnsupportedOperation!");
+        }
+
+        public void usefulOperation() {
+            System.out.println("Executing useful operation.");
+        }
+
+        @Override
+        public void close() throws Exception {
+            System.out.println("Closing database connection...");
+        }
+    }
+
+        public static class DBConnectionManager {
+            public FakeConnection getFakeConnection() {
+
+                return new FakeConnection();
+            }
+        }
+}
