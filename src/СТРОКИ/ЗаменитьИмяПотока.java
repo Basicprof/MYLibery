e267c57057126da4161cfe21da8f396bfc85a75b
Igor <Basicprof@gmail.com>
2018-04-22 17:59:19 +0300

    ЗаменитьИмя Нити

---
+++ src/СТРОКИ/ЗаменитьИмяПотока.java
@@ -0,0 +1,53 @@
+import java.util.TimerTask;
+
+
+    public class ЗаменитьИмяПотока extends TimerTask {
+        protected TimerTask original;
+        protected final Thread.UncaughtExceptionHandler handler;
+
+        public ЗаменитьИмяПотока(TimerTask original) {
+            if (original == null) {
+                throw new NullPointerException();
+            }
+            this.original = original;
+            //init handler here
+            class  UncaughtExceptionHandler implements
+                    Thread.UncaughtExceptionHandler{
+                @Override
+                public void uncaughtException(Thread t, Throwable e) {
+                    String stars = t.getName().replaceAll(".", "*");
+                    System.out.println( e.getMessage().replaceAll(t.getName(), stars) );
+                    // Заменить имя треда
+                    // или часть строки на символ "*"
+
+                }
+            }  handler = new UncaughtExceptionHandler();
+            Thread.setDefaultUncaughtExceptionHandler(handler);
+
+        }
+
+        public void run() {
+            try {
+                original.run();
+            } catch (Throwable cause) {
+                Thread currentThread = Thread.currentThread();
+                handler.uncaughtException(
+                        currentThread,
+                        new Exception("Blah " + currentThread.getName()
+                                + " blah-blah-blah", cause));
+            }
+        }
+
+        public long scheduledExecutionTime() {
+            return original.scheduledExecutionTime();
+        }
+
+        public boolean cancel() {
+            return original.cancel();
+        }
+
+        public static void main(String[] args) {
+
+
+        }
+    }
