6b81fef365ec7bb5d900b98d8a1e930926a2dcde
Basicprof <Basicprof@gmail.com>
2018-05-25 20:20:08 +0300

    Конвертер систем счислений

--- src/ПОТОКИ/НИТИ/ЗапускОжиданиеОчередь.java
+++ src/ПОТОКИ/НИТИ/ЗапускОжиданиеОчередь.java
@@ -1,8 +1,7 @@
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-
-public class ЗапускОжиданиеОчередь {
+ public class ЗапускОжиданиеОчередь {
     public static void main(String[] args) throws InterruptedException {
         //Add your code here
         LinkedBlockingQueue<Runnable> linkedBlockingQueue = new LinkedBlockingQueue<>();
@@ -11,16 +10,14 @@ public class ЗапускОжиданиеОчередь {
             linkedBlockingQueue.add(new Runnable() {
                 @Override
                 public void run() {
-
-                    doExpensiveOperation(finalI); }
+              doExpensiveOperation(finalI); }
             });
         }
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(3,5,1000, TimeUnit.MILLISECONDS,linkedBlockingQueue); // Создаем пул потоков с основными 3 максимум 5 время удаления потока при простое, и очередь в которую складываются задачи
-        threadPoolExecutor.prestartAllCoreThreads();//Запусти все трэды, которые входят в основное кол-во трэдов - ядро
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(3, 5,1000, TimeUnit.MILLISECONDS,linkedBlockingQueue);                                       // Создаем пул потоков с основными 3 максимум 5 время удаления потока при простое, и очередь в    которую складываются задачи
+        threadPoolExecutor.prestartAllCoreThreads();                                                                 //Запусти все трэды, которые входят в основное кол-во трэдов - ядро
         threadPoolExecutor.shutdown();//Запрети добавление новых задач на исполнение в пул
-        threadPoolExecutor.awaitTermination(5,TimeUnit.SECONDS);//5 секунд на завершение всех тасок
-
-        /* output example
+        threadPoolExecutor.awaitTermination(5,TimeUnit.SECONDS);                                              //5 секунд на завершение всех тасок
+      /* output example
 pool-1-thread-2, localId=2
 pool-1-thread-3, localId=3
 pool-1-thread-1, localId=1
@@ -32,9 +29,8 @@ pool-1-thread-3, localId=9
 pool-1-thread-2, localId=8
 pool-1-thread-1, localId=10
          */
-    }
-
-    private static void doExpensiveOperation(int localId) {
+        }
+   private static void doExpensiveOperation(int localId) {
         System.out.println(Thread.currentThread().getName() + ", localId=" + localId);
     }
 }
